78f77926cb0e6168df8edf2f2ba891ecdb57e2ed
almog-sharoni <65913065+almog-sharoni@users.noreply.github.com>
2023-06-28 17:28:29 +0300

    lobby

---
+++ src/blackJackApp/LobbyController.java
@@ -0,0 +1,21 @@
+package blackJackApp;
+
+import java.awt.event.ActionEvent;
+import javax.swing.SwingUtilities;
+import java.awt.event.ActionListener;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
+
+public class LobbyController {
+
+    private Lobby theLobby;
+    private LobbyWindow theLobbyWindow;
+
+    public void addWindow(LobbyWindow newWindow) {
+        this.theLobbyWindow = newWindow;
+
+    }
+
+
+
+}
