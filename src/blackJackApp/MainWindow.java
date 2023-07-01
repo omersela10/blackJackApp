2c2d746f53180582eb95e93e3d2ecdd05a268c56
omersela10 <90277998+omersela10@users.noreply.github.com>
2023-07-01 20:02:18 +0300

    update

--- src/blackJackApp/MainWindow.java
+++ src/blackJackApp/MainWindow.java
@@ -66,7 +66,7 @@ public class MainWindow extends JFrame {
         signUpButton.setBounds(baffer, 500, 120, 30);
         logInButton.setBounds(baffer+150, 500, 120, 30);
         playAsGuestButton.setBounds(baffer+300, 500, 120, 30);
-        leaderboardButton.setBounds(50, 700, 120, 30);
+        leaderboardButton.setBounds(baffer - 200, 600, 120, 30);
 
         // Add buttons to the mainPanel
         mainPanel.add(signUpButton);
