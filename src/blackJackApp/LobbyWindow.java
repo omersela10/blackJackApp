911bd1f69f9e0d1ee45681bf8c790d43753c605e
almog-sharoni <65913065+almog-sharoni@users.noreply.github.com>
2023-07-02 12:12:49 +0300

    lobby design

--- src/blackJackApp/LobbyWindow.java
+++ src/blackJackApp/LobbyWindow.java
@@ -2,14 +2,13 @@ package blackJackApp;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
@@ -20,7 +19,6 @@ public class LobbyWindow extends JFrame{
 
     // Data Members
     private final Player thePlayer;
-    private JLabel playerNameField;
     private final TableController tcOneHundred;
     private final TableController tcFifty;
     private final TableController tcFive;
@@ -54,12 +52,9 @@ public class LobbyWindow extends JFrame{
 	}
 
 
-	public void createAndShowGUI() {
-
-
+    public void createAndShowGUI() {
         setTitle("Lobby Window");
-        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
         // Create the panel with a BorderLayout
         // Load and draw the background image
@@ -68,66 +63,42 @@ public class LobbyWindow extends JFrame{
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 // Load and draw the background image
-                ImageIcon backgroundIcon = new ImageIcon("resources/table/LobbyWindow.png");
+                ImageIcon backgroundIcon = new ImageIcon("resources/lobbyWindow/lobbyBG.png");
                 Image image = backgroundIcon.getImage();
                 g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
             }
         };
-        
-    
+
         this.panel.setLayout(new BorderLayout());
 
         // Create a label for the player's name and total money
-        this.playerNameField = new JLabel("Name: " + thePlayer.getPlayerName() + " | Money: " + thePlayer.getTotalMoney() + "$");
-        playerNameField.setBounds(20, 20, 500, 20);
-        playerNameField.setForeground(Color.GRAY);
-        this.panel.add(playerNameField, BorderLayout.NORTH);
+        JLabel playerNameField = new JLabel("Name: " + thePlayer.getPlayerName() + " | Money: " + thePlayer.getTotalMoney() + "$");
+        playerNameField.setForeground(Color.WHITE);
+
+//        playerNameField.setHorizontalAlignment(SwingConstants.CENTER);
+        playerNameField.setFont(new Font("Arial", Font.BOLD, 14));
+        this.panel.add(playerNameField, BorderLayout.PAGE_START);
 
         // Add components to the panel
-  
         this.panel.add(createTableButtonsPanel(), BorderLayout.CENTER);
         this.panel.add(createActionButtonPanel(), BorderLayout.SOUTH);
 
-        WindowListener windowListener = new WindowAdapter(){
-            
+        WindowListener windowListener = new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
                 // Handle the close event here
-            	updateUserConnectInDB(false);
+                updateUserConnectInDB(false);
             }
-
-			
         };
         this.addWindowListener(windowListener);
 
-    			
-
-  
-	
-        
-        
         // Set the panel as the content pane of the frame
         this.setContentPane(panel);
-        // Set the size of the window
-        this.setSize(800, 600); // Set the width to 800 pixels and height to 600 pixels
-
-        // Center the window on the screen
+        this.pack();
         this.setLocationRelativeTo(null);
-
         this.setVisible(true);
     }
 
-    private void createPlayerNamePanel() {
-    	
-    	this.panel.remove(this.playerNameField);
-    
-    	// Create a label for the player's name and total money
-    	 this.playerNameField = new JLabel("Name: " + thePlayer.getPlayerName() + " | Money: " + thePlayer.getTotalMoney() + "$");
-    	 this.playerNameField.setBounds(20, 20, 500, 20);
-    	 this.playerNameField.setForeground(Color.GRAY);
-    	 this.panel.add(this.playerNameField, BorderLayout.NORTH);
- 
-    }
 
     private JPanel createTableButtonsPanel() {
     	
@@ -145,32 +116,27 @@ public class LobbyWindow extends JFrame{
 
         JButton button = new JButton(buttonText);
         button.setFont(new Font("Arial", Font.BOLD, 18));
-        button.setForeground(Color.WHITE);
+        button.setForeground(Color.BLACK);
         button.setBackground(Color.DARK_GRAY);
-        button.setFocusPainted(false);
-        button.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-            	
-                // Handle button click event
-                TableWindow nTw = new TableWindow(thePlayer);
-                switch (buttonText) {
-                    case TABLESTRING100:
-                    	 tcOneHundred.addWindow(nTw);
-                    	 tcOneHundred.getTable().setTableController(tcOneHundred);
-                    	 break;
-                    case TABLESTRING50:
-                    	 tcFifty.addWindow(nTw);
-                    	 tcFifty.getTable().setTableController(tcFifty);
-                    	 break;
-                    case TABLESTRING5:
-                    	 tcFive.addWindow(nTw);
-                    	 tcFive.getTable().setTableController(tcFive);
-                    	 break;
+        button.addActionListener(e -> {
+
+            // Handle button click event
+            TableWindow nTw = new TableWindow(thePlayer);
+            switch (buttonText) {
+                case TABLESTRING100 -> {
+                    tcOneHundred.addWindow(nTw);
+                    tcOneHundred.getTable().setTableController(tcOneHundred);
                 }
-                setVisible(false);
+                case TABLESTRING50 -> {
+                    tcFifty.addWindow(nTw);
+                    tcFifty.getTable().setTableController(tcFifty);
+                }
+                case TABLESTRING5 -> {
+                    tcFive.addWindow(nTw);
+                    tcFive.getTable().setTableController(tcFive);
+                }
             }
+            setVisible(false);
         });
         return button;
     }
@@ -180,70 +146,67 @@ public class LobbyWindow extends JFrame{
         JPanel actionButtonPanel = new JPanel();
         actionButtonPanel.setOpaque(false);
         actionButtonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 10));
-        actionButtonPanel.add(createActionButton("Add Money"));
+        //actionButtonPanel.setBackground(new Color(131, 34, 34));
+        actionButtonPanel.add(createActionButton());
         return actionButtonPanel;
     }
 
-    private JButton createActionButton(String buttonText) {
-    	
-        JButton button = new JButton(buttonText);
+    private JButton createActionButton() {
+        JButton button = new JButton("Add Money");
         button.setFont(new Font("Arial", Font.BOLD, 16));
-        button.setForeground(Color.WHITE);
-        button.setBackground(new Color(59, 89, 182));
-        button.setFocusPainted(false);
-        button.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-            	
-                // Handle button click event
-                if(thePlayer instanceof UserPlayer) {
-                	addMoneyDialog();
-                
-                }
-                else {
-                	JOptionPane.showMessageDialog(null, "Guest players can not add money :(");
-                
-                }
+        button.setForeground(Color.BLACK);
+        button.setBackground(new Color(131, 34, 34));
+        button.addActionListener(e -> {
+            // Handle button click event
+//            if (true) {
+            if (thePlayer instanceof UserPlayer) {
+                addMoneyDialog();
+            } else {
+                JOptionPane.showMessageDialog(null, "Guest players cannot add money :(");
             }
         });
         return button;
     }
     public void addMoneyDialog() {
-    	
-        // Create a panel to hold the sign-up form components
+        // Create a panel to hold the slider and label
         JPanel addMoneyPanel = new JPanel();
         addMoneyPanel.setLayout(new BoxLayout(addMoneyPanel, BoxLayout.Y_AXIS));
 
-        // Create labels and text fields for name and password
-        JLabel amountLabel = new JLabel("Amount to add:");
-        JTextField amountTextField = new JTextField(20);
+        // Create a slider for selecting the amount to add
+        JSlider amountSlider = new JSlider(JSlider.HORIZONTAL, 0, 10000, 0);
+        amountSlider.setMajorTickSpacing(1000);
+        amountSlider.setMinorTickSpacing(50);
+       // amountSlider.setPaintTicks(true);
+        //amountSlider.setPaintLabels(true);
 
+        // Set the UI of the slider to use a custom knob image
+        ImageIcon knobIcon = new ImageIcon("resources/lobbyWindow/chip.png");
 
-        // Add the components to the sign-up panel
+        amountSlider.setUI(new CustomSliderUI(knobIcon));
+
+        // Create a label to show the selected amount
+        JLabel amountLabel = new JLabel("Amount: " + amountSlider.getValue());
+        amountLabel.setHorizontalAlignment(JLabel.CENTER);
+
+        // Add a change listener to update the label when the slider value changes
+        amountSlider.addChangeListener(e -> amountLabel.setText("Amount: " + amountSlider.getValue()));
+
+        // Add the slider and label to the panel
+        addMoneyPanel.add(amountSlider);
         addMoneyPanel.add(amountLabel);
-        addMoneyPanel.add(amountTextField);
 
-        // Show the sign-up panel and handle the button press
-        int result = JOptionPane.showConfirmDialog(this, addMoneyPanel, "Sign Up", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+        // Show the panel and handle the button press
+        int result = JOptionPane.showConfirmDialog(this, addMoneyPanel, "Add Money", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
 
         // If the OK button is pressed
         if (result == JOptionPane.OK_OPTION) {
-            // Update player money in DB
-        	String amount = amountTextField.getText();
-        	boolean isValid = amount.matches("^[1-9]\\d*$");
-        	if (isValid == true) {
-        		
-                thePlayer.setTotalMoney(thePlayer.getTotalMoney() + Integer.parseInt(amount));//Integer.parseInt(amountTextField.getText()));
-                DBManager.updateUserValues(((UserPlayer)thePlayer).getUser());
-                createPlayerNamePanel();
-                panel.revalidate();
-        	}
-        	else {
-        		JOptionPane.showMessageDialog(null, "Insert valid number, please");
-        	}
-
+            // Update player money in DB and update the UI
+            thePlayer.setTotalMoney(thePlayer.getTotalMoney() + amountSlider.getValue());
+            DBManager.updateUserValues(((UserPlayer) thePlayer).getUser());
+            panel.revalidate();
         }
-
     }
+
+
+
 }
